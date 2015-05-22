e50ea0681c04f46ad9f4151822609d9fef5c6de1
Ronny Coolen <ronny@king-foo.be>
2015-05-22 12:58:10 +0200

    fixed callback

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ var checkBroodjes = function(html, callback) {
     if (html.match(/Ja/)) {
         if (broodjes == false) {
             broodjes = true;
+            console.log('De broodjes zijn er!');
             return callback();
+        } else {
+            console.log('De broodjes zijn er, maar het bericht werd al verstuurd.');
         }
     } else {
         broodjes = false;
+        console.log('Nog geen broodjes.');
     }
 }
 
@@ -23,53 +27,48 @@ setInterval(function() {
             response += chunk;
         });
         res.on('end', function() {
-            checkBroodjes(response, function(send) {
-                if (send) {
-                    console.log('De broodjes zijn er!');
-                    params = {
-                        token: config.slack.token,
-                        channel: config.slack.channel,
-                        text: "De broodjes zijn er!",
-                        as_user: false,
-                        username: "BroodjesBot"
-                    };
+            checkBroodjes(response, function() {
+                params = {
+                    token: config.slack.token,
+                    channel: config.slack.channel,
+                    text: "De broodjes zijn er!",
+                     as_user: false,
+                    username: "BroodjesBot"
+                };
 
-                    post_data = querystring.stringify(params);
-                    options = {
-                        hostname: 'slack.com',
-                        method: 'POST',
-                        path: '/api/chat.postMessage',
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                            'Content-Length': post_data.length
+                post_data = querystring.stringify(params);
+                options = {
+                    hostname: 'slack.com',
+                    method: 'POST',
+                    path: '/api/chat.postMessage',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Content-Length': post_data.length
+                    }
+                };
+
+                req = https.request(options);
+
+                req.on('response', function(res) {
+                    var buffer = '';
+                    res.on('data', function(chunk) {
+                         return buffer += chunk;
+                    });
+                    return res.on('end', function() {
+                        var value;
+                        if (res.statusCode === 200) {
+                            value = JSON.parse(buffer);
+                            console.log(value);
+                        } else {
+                            console.log('error!');
                         }
-                    };
-
-                    req = https.request(options);
-
-                    req.on('response', function(res) {
-                        var buffer = '';
-                        res.on('data', function(chunk) {
-                             return buffer += chunk;
-                        });
-                        return res.on('end', function() {
-                            var value;
-                            if (res.statusCode === 200) {
-                                value = JSON.parse(buffer);
-                                console.log(value);
-                            } else {
-                                console.log('error!');
-                            }
-                        });
                     });
-                    req.on('error', function(error) {
-                        console.log('error');
-                    });
-                    req.write(post_data);
-                    req.end();
-                } else {
-                    console.log('Nog geen broodjes');
-                }
+                });
+                req.on('error', function(error) {
+                    console.log('error');
+                });
+                req.write(post_data);
+                req.end();
             });
         });
     }).on('error', function(e) {
